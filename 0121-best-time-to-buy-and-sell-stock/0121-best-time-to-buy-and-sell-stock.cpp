f890f59c5a4189f92ad57240faca407436e542d3
Mohamed-Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-02-25 16:27:47 +0200

    Time: 165 ms (18.74%), Space: 93.4 MB (11.82%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int ans=0,mn=prices[0];
+        for(auto x:prices){
+            ans=max(ans,x-mn);
+            mn=min(mn,x);
+        }
+        return ans;
+    }
+};
