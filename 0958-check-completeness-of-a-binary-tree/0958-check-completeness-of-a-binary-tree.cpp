405226f8d0bbc55916b5def43e867d28fe7e9ea5
Mohamed-Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-03-15 12:57:59 +0200

    Time: 10 ms (19.69%), Space: 10.8 MB (6.36%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.cpp
@@ -12,27 +12,19 @@
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
-        int h=CountLevel(root);
-        vector<int>LastLevel;
-        bool ans= solve(root,1,h,LastLevel);
-        if(LastLevel.size()==0)return ans;
-        for(int i=0;i<LastLevel.size()-1;i++){
-            if(LastLevel[i]==0 && LastLevel[i+1]!=0)return false;
+        vector<TreeNode*>vec{root};
+        bool ans=false;
+        for(int i=0;i<vec.size();i++){
+            TreeNode* node=vec[i];
+            if(node!=nullptr){
+                vec.push_back(node->left);
+                vec.push_back(node->right);
+                if(ans)
+                    return false;
+            }
+            else
+                ans=true;
         }
         return ans;
-    }
-    int CountLevel(TreeNode* root){
-        if(root==nullptr)return 0;
-        return 1+max(CountLevel(root->left),CountLevel(root->right));
-    }
-    bool solve(TreeNode* root,int curlevel,int h,vector<int>& LastLevel){
-        if(root==nullptr)return true;
-        if(curlevel<h-1 && (root->left==nullptr || root->right==nullptr))return false;
-        if(curlevel==h-1){
-            LastLevel.push_back(root->left==nullptr?0:root->left->val);
-            LastLevel.push_back(root->right==nullptr?0:root->right->val);
-        }
-        
-        return solve(root->left,curlevel+1,h,LastLevel) && solve(root->right,curlevel+1,h,LastLevel);
     }
 };
