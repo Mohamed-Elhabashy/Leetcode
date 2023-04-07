e623c1026f13f286367e073b454129b9a153c312
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-04-07 03:22:22 +0200

    Time: 86 ms (33.53%), Space: 30.7 MB (45.59%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int a[4]={1,-1,0,0};
+    int b[4]={0,0,-1,1};
+    vector<vector<bool>>vis;
+    int n,m;
+    bool vaild(int i,int j,vector<vector<int>>& grid){
+        return i>=0 && i<n && j>=0 && j<m ;
+    }
+    int numEnclaves(vector<vector<int>>& grid) {
+        n=grid.size();m=grid[0].size();
+        vis.resize(n,vector<bool>(m,0));
+        int ans=0;
+        for(int i=1;i<n-1;i++){
+            for(int j=1;j<m-1;j++){
+                if(!vis[i][j] && grid[i][j]==1){
+                    int count=0;
+                    if(dfs(i,j,grid,count)){
+                        ans+=count;
+                    }
+                }
+            }
+        }
+        return ans;
+    }
+    bool dfs(int i,int j,vector<vector<int>>& grid,int& count){
+        if(!vaild(i,j,grid))return 0;
+        if(vis[i][j] || grid[i][j]==0)return 1;
+        vis[i][j]=1;
+        count++;
+        bool flag=1;
+        for(int x=0;x<4;x++){
+            int ii=i+a[x],jj=j+b[x];
+            flag&=dfs(ii,jj,grid,count);
+            
+        }
+        return flag;
+    }
+};
