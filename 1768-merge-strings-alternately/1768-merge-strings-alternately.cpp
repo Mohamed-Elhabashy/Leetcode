7efc2a74e773810958f50ab2f22eb5f2c9ee56d9
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-04-18 02:44:02 +0200

    Time: 0 ms (100.00%), Space: 6.1 MB (98.18%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        string ans="";
+        for(int i=0;i<min(word1.size(),word2.size());i++){
+            ans+=word1[i];
+            ans+=word2[i];
+        }
+        if(word1.size()>word2.size())
+            ans+=word1.substr(word2.size());
+        if(word2.size()>word1.size())
+            ans+=word2.substr(word1.size());
+        return ans;
+    }
+};
