1ec2715b19bc1a1fc0ec60a889416300a159277c
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-05-07 13:48:39 +0300

    Time: 3452 ms (86.89%), Space: 0B (100.00%) - LeetHub

--- 1075-project-employees-i/1075-project-employees-i.sql
+++ 1075-project-employees-i/1075-project-employees-i.sql
@@ -1,5 +1,5 @@
 select 
-P.project_id ,ROUND(avg(cast(E.experience_years as decimal)),2) as average_years 
+P.project_id ,ROUND(avg(E.experience_years * 1.0),2) as average_years 
 from Project as P 
 left join Employee as E
 on P.employee_id = E.employee_id
