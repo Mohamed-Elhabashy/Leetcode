be83fe93a130f23b9d57451eaf6384e7b8551b12
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-05-09 11:10:20 +0300

    Time: 4528 ms (94.05%), Space: 0B (100.00%) - LeetHub

--- 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
+++ 1633-percentage-of-users-attended-a-contest/1633-percentage-of-users-attended-a-contest.sql
@@ -1,6 +1,6 @@
-select r.contest_id, round((count(*)*100.0/(select count(*) from Users)) , 2) as percentage
-from Users u
-join Register r
-on u.user_id = r.user_id
-group by r.contest_id
-order by percentage desc, r.contest_id;
+select R.contest_id,ROUND((count(*)*100.00/(SELECT COUNT(*) FROM Users)),2) as percentage 
+from Users as U 
+join Register as R
+on U.user_id = R.user_id
+group by R.contest_id
+order by percentage DESC , R.contest_id
