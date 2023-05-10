19e8dbac61bb988d1fd19563dbc203ad857f6489
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-05-10 14:16:05 +0300

    Time: 474 ms (96.75%), Space: 0B (100.00%) - LeetHub

--- 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -1,6 +1,6 @@
 /* Write your T-SQL query statement below */
 select p.product_name ,sum(o.unit) as unit
-from products as p join Orders as o 
+from products as p left join Orders as o 
 on p.product_id  = o.product_id  and o.order_date >='2020-02-01' and o.order_date <='2020-02-29'
 group by p.product_id,p.product_name
 having sum(o.unit)>=100
