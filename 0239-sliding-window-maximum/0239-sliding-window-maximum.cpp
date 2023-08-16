48036704b33ce27979ededce7fe6b8384ef8eb9c
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-08-16 16:10:47 +0300

    Time: 784 ms (5.04%), Space: 163.1 MB (18.39%) - LeetHub

---
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    unordered_map<int,int>mp;
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) {
+        priority_queue<pair<int,int>>pq;
+        for(int i=0;i<k;i++){
+            pq.push({nums[i],i});
+            mp[nums[i]]++;
+        }        
+        vector<int>ans;
+        ans.push_back(pq.top().first);
+        for(int i=k,j=0;i<nums.size();i++,j++){
+            cout<<i<<" "<<j<<endl;
+            mp[nums[j]]--;
+            mp[nums[i]]++;
+            while(pq.size() && pq.top().second<=j)pq.pop();
+            pq.push({nums[i],i});
+            ans.push_back(pq.top().first);           
+
+        }
+        return ans;
+    }
+};
