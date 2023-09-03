e38c5b10365784465c81e98cdee9efee5de4e6ae
Mohamed Elhabashy <59234763+Mohamed-Elhabashy@users.noreply.github.com>
2023-09-03 03:13:08 +0300

    Time: 470 ms (10.00%), Space: 125.8 MB (30.00%) - LeetHub

---
+++ 2842-count-k-subsequences-of-a-string-with-maximum-beauty/2842-count-k-subsequences-of-a-string-with-maximum-beauty.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int MOD=1e9+7;
+    map<pair<int,pair<int,int>>,unsigned long long>mp;
+    int countKSubsequencesWithMaxBeauty(string s, int k) {
+        map<char,int>m;
+        for(auto x:s)m[x]++;
+        if(m.size()<k)return 0;
+        vector<int>v;
+        for(char i='a';i<='z';i++){
+            if(m[i]>0)
+                v.push_back(m[i]);
+              
+        }
+        sort(v.rbegin(),v.rend());
+        int mx=0;
+        for(int i=0;i<k;i++)mx+=v[i];
+        return solve(0,k,mx,v);
+    }
+    unsigned long long solve(int i, int remK, int remS,vector<int>& v) {
+        if(remK==0 && remS==0)return 1;
+        if(i==v.size() || remK<=0 || remS<=0)return 0;
+        if(mp.count({i,{remK,remS}}))return mp[{i,{remK,remS}}];
+        
+        unsigned long long& ret=mp[{i,{remK,remS}}];
+        ret%=MOD;
+        ret=solve(i+1,remK,remS,v)%MOD;
+        ret+=((v[i]%MOD)*(solve(i+1,remK-1,remS-v[i],v)%MOD))%MOD;
+        ret%=MOD;
+        return ret;
+    }
+};
